e9c3b091b6768e39b2fe1a1aa3f6af1a91a99c0e
Karen G <kgraves0701@gmail.com>
2018-03-14 18:59:28 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,11 @@
 def square_array(array)
-  squared_array = []
-  array.each do |element|
-    element2 = element * element
-    squared_array << element2
-  end
-  squared_array
+  # squared_array = []
+  # array.each do |element|
+  #   element2 = element * element
+  #   squared_array << element2
+  # end
+  # squared_array
+  array.collect {|x| x*x}
 end
 
 def square_with_collect(array)
