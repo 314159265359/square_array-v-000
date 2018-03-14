dd3a6843db6e1307f49c2e599aaf049d88da68a7
Karen G <kgraves0701@gmail.com>
2018-03-14 18:56:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |element|
+    element2 = element * element
+    squared_array << element2
+  end
+  squared_array
 end
